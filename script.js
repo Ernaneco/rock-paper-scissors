07c93dd8a45a10c76980ee4f9a42aaae4a1d13c7
Ernaneco <104723811+Ernaneco@users.noreply.github.com>
2025-10-29 17:56:59 -0300

    Create functions getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    const numberChoice = Math.floor(Math.random() * 3);
+    if (numberChoice === 0) {
+        return "rock"
+    } else if (numberChoice === 1) {
+        return "paper"
+    } else if (numberChoice === 2) {
+        return "scissor"
+    }
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Choose rock/paper/scissor: ");
+    console.log(humanChoice);
+}
+
+let humanScore = 0;
+let computerScore = 0;
